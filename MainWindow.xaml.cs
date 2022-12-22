6ab7559b4fd0ade830302fd9833d9b18d729081b
yannicklal <yannicklal@free.fr>
2022-12-22 15:00:44 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Xps.Packaging;
+using System.Xml.Linq;
+
+namespace XpsTest
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var xps = new XpsDocument("printPreview.xps", FileAccess.Read);
+            viewer.Document = xps.GetFixedDocumentSequence();
+
+            xps.Close();
+        }
+    }
+}
